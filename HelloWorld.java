2482e90af26421372fa7e873998be81c5fdbad68
srbh-afk <53375643+srbh-afk@users.noreply.github.com>
2019-08-08 16:50:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld
 {
   public static void main()
   {
-    String first_name = "Saurabh";
     String last_name = "Kumar";
     System.out.println("Hello " + first_name + " " + last_name);
   }
